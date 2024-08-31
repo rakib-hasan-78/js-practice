1d0f881663e59a6c139e21eeab56deaa5f416bec
rakib-hasan-78 <rakib.webdev78@gmail.com>
2024-08-31 17:43:43 +0600

    array done

--- main.js
+++ main.js
@@ -9,158 +9,158 @@ document.querySelector('#app').innerHTML = `
       <h1>hello world !!!!!</h1>
   </div>
 `
-numbers(); // lesson 1
-strings(); // lesson 2
-booleans(); //lesson 3
+// numbers(); // lesson 1
+// strings(); // lesson 2
+// booleans(); //lesson 3
 
 
 
-const n = 100;
-const x = + '1000'
-console.log(typeof x)
+// const n = 100;
+// const x = + '1000'
+// console.log(typeof x)
 
-var r = 21
-var y = 12
-console.log(r%y)
+// var r = 21
+// var y = 12
+// console.log(r%y)
 
-r++;
-console.log(++r)
+// r++;
+// console.log(++r)
 
 
-// math function  
-let math = 22.95214;
-let result = Math.abs(math);
-console.log(result);
-const round = Math.round(math)
-console.log(round);
-let floor = Math.floor(math);
-console.log(floor);
-let ceil = Math.ceil(math)
-console.log(ceil)
-console.log(Math.max(103,2382,304))
-console.log(Math.min(1231, 313,42,11,111))
-console.log(Math.cbrt(27))
+// // math function  
+// let math = 22.95214;
+// let result = Math.abs(math);
+// console.log(result);
+// const round = Math.round(math)
+// console.log(round);
+// let floor = Math.floor(math);
+// console.log(floor);
+// let ceil = Math.ceil(math)
+// console.log(ceil)
+// console.log(Math.max(103,2382,304))
+// console.log(Math.min(1231, 313,42,11,111))
+// console.log(Math.cbrt(27))
 
-console.log(Math.pow(2,3))
+// console.log(Math.pow(2,3))
 
-// time function 
+// // time function 
 
-const date = new Date();
-console.log(date.getTime(),  date.getMilliseconds())
+// const date = new Date();
+// console.log(date.getTime(),  date.getMilliseconds())
 
 
 
 // js conditions 
 
-let x1 = 41;
-let mod = 13;
+// let x1 = 41;
+// let mod = 13;
 
-if (!(x1%mod===2)) {
-    console.log('no bhaghsesh is 2')
-} else {
-  console.log('got a number !!!')
-}
+// if (!(x1%mod===2)) {
+//     console.log('no bhaghsesh is 2')
+// } else {
+//   console.log('got a number !!!')
+// }
 
-if (!(mod>x1)) {
-    console.log('prime number is ' + x1 )
-} else {
-  console.log('invalid number mod' + '(' + mod + ') + is larger number')
-}
+// if (!(mod>x1)) {
+//     console.log('prime number is ' + x1 )
+// } else {
+//   console.log('invalid number mod' + '(' + mod + ') + is larger number')
+// }
 
-mod%2===0 ? console.log('${mod} is an even number'): console.log(`${mod} is an odd number`);
+// mod%2===0 ? console.log('${mod} is an even number'): console.log(`${mod} is an odd number`);
 
-let wx = 0
-switch (true) {
-  case (wx<0):
-  console.log(`${wx} is an invalid number`)
-  break;
-  case (wx===0):
-  console.log(`${wx} is not a number`)
-  break;
-  case (wx%1!==0):
-  console.log(`${wx} is  a  float number`)
-  break;
-  case (wx%2===0):
-  console.log(`${wx} is  an even number`)
-  break;
-  case (wx%2===1):
-  console.log(`${wx} is  an odd number`)
-  break;
+// let wx = 0
+// switch (true) {
+//   case (wx<0):
+//   console.log(`${wx} is an invalid number`)
+//   break;
+//   case (wx===0):
+//   console.log(`${wx} is not a number`)
+//   break;
+//   case (wx%1!==0):
+//   console.log(`${wx} is  a  float number`)
+//   break;
+//   case (wx%2===0):
+//   console.log(`${wx} is  an even number`)
+//   break;
+//   case (wx%2===1):
+//   console.log(`${wx} is  an odd number`)
+//   break;
 
-  default:
-  console.log(`invalid number`)
-  break;
-}
+//   default:
+//   console.log(`invalid number`)
+//   break;
+// }
 
-const d = new Date();
-const day = d.getDay();
-switch (day) {
-  case 0:
-  console.log(`Today is Sunday`)
-  break;
-  case 1:
-  console.log(`Today is Monday`)
-  break;
-  case 2:
-  console.log(`Today is Tuesday`)
-  break;
-  case 3:
-  console.log(`Today is Wednesday`)
-  break;
-  case 4:
-  console.log(`Today is Thursday`)
-  break;
-  case 5:
-  console.log(`Today is Friday`)
-  break;
-  case 6:
-  console.log(`Today is Saturday`)
-  break;
+// const d = new Date();
+// const day = d.getDay();
+// switch (day) {
+//   case 0:
+//   console.log(`Today is Sunday`)
+//   break;
+//   case 1:
+//   console.log(`Today is Monday`)
+//   break;
+//   case 2:
+//   console.log(`Today is Tuesday`)
+//   break;
+//   case 3:
+//   console.log(`Today is Wednesday`)
+//   break;
+//   case 4:
+//   console.log(`Today is Thursday`)
+//   break;
+//   case 5:
+//   console.log(`Today is Friday`)
+//   break;
+//   case 6:
+//   console.log(`Today is Saturday`)
+//   break;
 
-  default:
-  console.log(`invalid day`)
-  break;
-}
+//   default:
+//   console.log(`invalid day`)
+//   break;
+// }
 
 
 //  it first converts its argument to a number using the Number() function
 
-let  sum = 0;
-let total = 0;
-for(let i =0; i<=20;i++){
-  if (i%2===0) {
-    console.log(sum + '+'+i +'='+(sum+i))
-    sum+=i;
-  }
-}
-console.log('Total sum = '+sum);
+// let  sum = 0;
+// let total = 0;
+// for(let i =0; i<=20;i++){
+//   if (i%2===0) {
+//     console.log(sum + '+'+i +'='+(sum+i))
+//     sum+=i;
+//   }
+// }
+// console.log('Total sum = '+sum);
 
-for (let i = 0; i <=20; i++) {
-  if (i%2===1) {
-    console.log(total + '+'+i+'='+ (total+i)); 
-    total+=i;
-  }
-}
-console.log('Total Odd Number Sumasion ='+total);
+// for (let i = 0; i <=20; i++) {
+//   if (i%2===1) {
+//     console.log(total + '+'+i+'='+ (total+i)); 
+//     total+=i;
+//   }
+// }
+// console.log('Total Odd Number Sumasion ='+total);
 
 
-let isTrue = true;
-while (isTrue) {
-  let numbers = Math.round(Math.random()*10);
-  if (numbers===7) {
-    console.log('winner winner chicken dinner!')
-    break;
-  } else {
-    console.log(`you got ${numbers}`)
-  }
-}
+// let isTrue = true;
+// while (isTrue) {
+//   let numbers = Math.round(Math.random()*10);
+//   if (numbers===7) {
+//     console.log('winner winner chicken dinner!')
+//     break;
+//   } else {
+//     console.log(`you got ${numbers}`)
+//   }
+// }
 
-let m = 0;
+// let m = 0;
 
-while (m<10) {
-  m++;
-  console.log(m)
-}
+// while (m<10) {
+//   m++;
+//   console.log(m)
+// }
 
 // function x11() {
 //   if (m<10) {
@@ -171,27 +171,135 @@ while (m<10) {
 // }
 // x11()
 
-let dd = 5;
-for (let i = 1; i<=dd; i++) {
-  let em = ''
+// let dd = 5;
+// for (let i = 1; i<=dd; i++) {
+//   let em = ''
     
-    for (let j = 1; j<=i; j++) {
+//     for (let j = 1; j<=i; j++) {
 
-        em += '* '
-    }
+//         em += '* '
+//     }
 
-    console.log(em);
+//     console.log(em);
+// }
+
+
+// const firstName = 'rakib';
+// const lastName = 'hasan';
+// const myName = firstName.concat(' ', lastName);
+// console.log(myName);
+// const results = firstName.padEnd(firstName.length+1,' ') + lastName
+// console.log(results)
+// console.log(myName.substring(6,11));
+
+// const p = `The government has appointed senior lawyer Ehsanul Haque Shomaji as Dhaka Metropolitan public prosecutor.`;
+// const pResult = p.split(' ').map(word=>word.charAt(0).toUpperCase()+word.slice(1)).join(' ')
+// console.log(pResult);
+
+
+
+// const xyz = `abcnsj ksjjhhavv ovakoiajv`;
+
+// let length = 0;
+
+// while(true){
+//   if (xyz.charAt(length)==='') {
+//     break
+//   } else {
+//     length++
+//   }
+// }
+// console.log(length)
+// console.log(xyz.length);
+
+// let textLastIndex = `The government has appointed senior lawyer Ehsanul Haque Shomaji as Dhaka Metropolitan public prosecutor`;
+
+// const textLastIndexSplitter = textLastIndex.split(' ').map(word=>word.slice(0, -1)+ word.charAt(word.length-1).toUpperCase()).join(' ');
+// console.log(textLastIndexSplitter);
+
+// let arr =[1,2,3,4,undefined,null,5,6,7,8,9,90];
+// let container = 0;
+
+// while (true) {
+//   if (arr[container]===undefined) {
+//       break
+//   }else {
+//     container++
+//   }
+// }
+// console.log(`array length is ${container}`)
+
+// while (container<arr.length) {
+//     container++
+// }
+
+// console.log(`array length is ${container}`)
+
+
+// let arr1 = [1,2,3,4,6,7,8,9];
+
+// let targetedIndex = 3
+// let assignedValue = 5
+// let leng = arr1.length-1
+
+// for (let i = leng; i>=targetedIndex; i--) {
+
+//     arr1[i + 1] = arr1[i]
+// }
+// arr1[targetedIndex] = assignedValue;
+
+// console.log(arr1)
+
+// for(let i = targetedIndex; i<leng; i++){
+//     arr1[i] = arr1[i + 1]
+// }
+// arr1.pop()
+
+// console.log(arr1);
+
+// let anotherArray = arr1.splice(2,4)
+// console.log(anotherArray);
+
+
+// let found  = false;
+// let data = 78
+
+// for (let i = 0 ; i<anotherArray.length-1; i++) {
+//   if (anotherArray[i]===data) {
+//     console.log(`data at index number ${i}`);
+//     found = true;
+//     break;
+//   }
+// }
+// if(!found){
+//   console.log(`${data} not found`)
+// }
+
+const ma = [
+  [1,2,3,4],
+  [5],
+  [6,7]
+];
+const resulta = ma.flat(3);
+console.log(resulta) 
+
+for (let i = 0; i < ma.length; i++) {
+  for(let j = 0; j<ma[i].length; j++){
+    
+      console.log(`values are ${ma[i][j]}`)
+    
+  }
+  
+} 
+const reverseArray = [1,2,3,4,5,6,7,8,9,10];
+for(let i =0; i<(reverseArray.length)/2; i++){
+  let temp = reverseArray[i];
+  reverseArray[i] = reverseArray[reverseArray.length-1-i];
+  reverseArray[reverseArray.length-1-i] = temp;
 }
+console.log(reverseArray);
 
 
-const firstName = 'rakib';
-const lastName = 'hasan';
-const myName = firstName.concat(' ', lastName);
-console.log(myName);
-const results = firstName.padEnd(firstName.length+1,' ') + lastName
-console.log(results)
-console.log(myName.substring(6,11));
 
-const p = `The government has appointed senior lawyer Ehsanul Haque Shomaji as Dhaka Metropolitan public prosecutor.`;
-const pResult = p.split(' ').map(word=>word.charAt(0).toUpperCase()+word.slice(1)).join(' ')
-console.log(pResult);
+const r = Array.from(reverseArray);
+console.log(r);
